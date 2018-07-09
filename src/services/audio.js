dad83014705444b4703656e9cf59b2317d40d001
ljhskyso <ljhskyso@gmail.com>
2018-07-09 01:14:57 -0700

    add sounds

--- src/services/audio.js
+++ src/services/audio.js
@@ -2,33 +2,18 @@
 /**
  * Simple wrapper on react-native-sound that exposes the sound effects used in the game.
  */
-// import ReactNativeSound from 'react-native-sound';
+import { Audio } from 'expo';
 
-const initializeSound = (name: string) => {
-  // return new ReactNativeSound(name, ReactNativeSound.MAIN_BUNDLE, err => {
-  //   if (err) {
-  //     console.warn('failed to load the sound', err);
-  //   }
-  // });
-};
-
-const playSound = sound => {
-  // sound.getCurrentTime(currentTime => {
-  //   if (currentTime === 0) {
-  //     sound.play();
-  //   } else {
-  //     sound.stop();
-  //     sound.play();
-  //   }
-  // });
-};
-
-const successSound = initializeSound('success.wav');
-const buttonSound = initializeSound('button.wav');
-const failureSound = initializeSound('failure.wav');
+async function playSound(asset) {
+  try {
+    await Audio.Sound.create(asset, { shouldPlay: true });
+  } catch (error) {
+    console.warn('failed to load the SUCCESS sound', err);
+  }
+}
 
 export default {
-  playSuccessSound: () => playSound(successSound),
-  playButtonSound: () => playSound(buttonSound),
-  playFailureSound: () => playSound(failureSound),
+  playSuccessSound: () => playSound(require('src/assets/audio/success.wav')),
+  playButtonSound: () => playSound(require('src/assets/audio/button.wav')),
+  playFailureSound: () => playSound(require('src/assets/audio/failure.wav')),
 };
