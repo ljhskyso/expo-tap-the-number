397c18db1be08c8a32f12266bf49a10a3f7985c0
ljhskyso <ljhskyso@gmail.com>
2018-07-09 00:39:29 -0700

    init core logic

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* @flow */
+import React, { Component } from 'react';
+import { Provider } from 'mobx-react/native';
+
+// import App from 'src/containers/App';
+// import GameStore from 'src/stores/game';
+// import RouterStore from 'src/stores/router';
+
+import { View } from 'react-native';
+
+// const gameStore = new GameStore();
+// const routerStore = new RouterStore();
+
+export class TapTheNumber extends Component {
+  render() {
+    return (
+      <View/>
+    );
+    // return (
+    //   <Provider game={gameStore} router={routerStore}>
+    //     <App />
+    //   </Provider>
+    // );
+  }
+}
+
+export default TapTheNumber;
